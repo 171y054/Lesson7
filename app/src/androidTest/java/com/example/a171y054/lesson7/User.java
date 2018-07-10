2b91a9fe55084b97495720ee09205e322f7da2b1
Naoki Hama <171y054@epson-isc.com>
2018-07-10 10:09:47 +0900

    initial commit

---
+++ app/src/androidTest/java/com/example/a171y054/lesson7/User.java
@@ -0,0 +1,26 @@
+package com.example.a171y054.lesson7;
+
+/**
+ * Created by 171y054 on 18/6/20.
+ */
+
+public class User {
+    long id;
+    String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
